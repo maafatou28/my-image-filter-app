e026d2b6c3cdfc0f832cb54d3dd21bf17498ba61
mame fatou diop <dmamefatou785@gmail.com>
2022-09-07 16:06:13 +0000

    adding filter image function

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Request,Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -28,6 +29,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    const {image_url} = req.query;
+    if (!image_url) {
+      return res.status(422).send({
+        message: `image_url query param is required`
+      })
+    }
+    const filtered_img_path = await filterImageFromURL(image_url.toString())
+    res.sendFile(filtered_img_path);
+
+    res.status(200).sendFile(filtered_img_path, () =>{
+      deleteLocalFiles([filtered_img_path]);
+    })
+  
+  })
+
 
   //! END @TODO1
   
